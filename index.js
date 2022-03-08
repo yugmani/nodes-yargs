7ce51828e4c2741dcfd8ce16448a95a087c66f1b
yugmani <yugmani@yahoo.com>
2022-03-07 22:54:44 -0800

    some codes on: process.argv

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-// run `node index.js` in the terminal
+// 1.
+// loading chalk package
+import chalk from 'chalk';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// 2.
+// run `node index.js` in the terminal
+// logs Node.js version
+// console.log(`Hello Node.js v${process.versions.node}!`);
+
+// 3.
+// to log an array of arguments provided:
+// i. logs path of node.js executable on your machine
+// ii. logs path of this file(index.js)
+// iii. logs other arguments provided
+// Note: to log extra argument enter this command in the command line: node index.js <extra argument>
+// eg. $ node index.js yoog
+// console.log(chalk.yellow(process.argv));
+
+// 4.
+// console.log(process.argv[0]); // logs first
+// console.log(process.argv[1]); // logs second
+// console.log(process.argv[2]); // logs third argument if provided.
+
+// 5.
+const command = process.argv[2];
+
+if (command === 'add') {
+  console.log('Adding a note!');
+} else if (command === 'remove') {
+  console.log('Removing a note!');
+} else {
+  console.log('Invalid argument!');
+}
